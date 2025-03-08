cdeefeb34a0853daeef95fc05d386a5afaf5439f
rashidmya <bugadev@proton.me>
2025-03-09 02:03:03 +0400

    moved provier

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Roboto } from "next/font/google";
 import "./globals.css";
 //
 import { SessionProvider } from "@/components/session-provider";
-import { ThemeProvider } from "@/components/theme-provider";
+import { ThemeProvider } from "@/components/providers/theme-provider";
 
 const acme = Roboto({
   subsets: ["latin"],
