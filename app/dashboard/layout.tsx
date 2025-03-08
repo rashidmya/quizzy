4892dea5a36484fd5a261d0c6da8ab9afabb5552
rashidmya <bugadev@proton.me>
2025-03-09 02:32:22 +0400

    moved sidebar to layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,7 +1,24 @@
+// components
+import { AppSidebar } from "@/components/app-sidebar";
+import {
+  SidebarInset,
+  SidebarProvider,
+} from "@/components/ui/sidebar";
+
+
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <div>{children}</div>;
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-4">
+         {children}
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  );
 }
