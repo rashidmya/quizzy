7a2d42dc9468de35fdfe99dcaa72e2e2ee963bdf
rashidmya <bugadev@proton.me>
2025-03-25 04:59:16 +0400

    --

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -2,14 +2,12 @@ import "next-auth";
 
 declare module "next-auth" {
   interface Session {
-    isQuiz?: boolean;
     user: {
       id: string;
     } & DefaultSession["user"];
   }
-
+  
   interface JWT {
-    isQuiz?: boolean;
     user: {
       id: string;
     } & DefaultSession["user"];
